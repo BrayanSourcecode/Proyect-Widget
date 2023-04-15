a40017b4986a5da6c2cf3ecc20261f3bb70c2bde
DevBrayan <brayanramirez4546@gmail.com>
2023-04-15 17:33:26 -0500

    adelanto de map-3d y map-base

--- widgets/prueba/src/runtime/components/map_default.tsx
+++ widgets/prueba/src/runtime/components/map_default.tsx
@@ -1,13 +1,13 @@
 // modulos de jimu core
 
 // modulos de react
-import React, { useRef } from "react"
-import { useEffect, useState } from "react"
+import React from "react"
+import { useEffect, useState, useRef, ChangeEvent } from "react"
 import "./map_default.css"
 
 
 // librerias de jimu argis
-import { JimuMapView, JimuMapViewConstructorOptions, MapViewManager } from "jimu-arcgis"
+import { JimuMapViewConstructorOptions, MapViewManager } from "jimu-arcgis"
 
 // modulos  de  api de js
 import MapView from "esri/views/MapView"
@@ -19,6 +19,9 @@ import { Switch } from 'jimu-ui'
 import "./map_default.css"
 
 import { IMConfig } from '../../config'
+import { map } from "esri/widgets/TableList/TableListViewModel"
+import BasemapGallery from "esri/widgets/BasemapGallery"
+import Expand from "esri/widgets/Expand"
 
 
 
@@ -36,83 +39,127 @@ function MapDefault(props: Myprosp) {
     const [view_actual, setView] = useState<__esri.View>(null);
     const [check_Map3D, setCheck_Map3D] = useState(false)
     const [check_MapBase, setCheck_MapBase] = useState(false)
+    const [check_Config_Map, setCheck_Config_Map] = useState(false)
+    const [check_Config_Basemap, setCheck_Config_BaseMap] = useState(false)
 
-// creamos el gestor de la views
+
+    // creamos el gestor de la views
     const mapViewManager = MapViewManager.getInstance();
-//creamos la referencia para el div del mapa 
+    //creamos la referencia para el div del mapa 
     const containerMap = useRef();
 
-// creamos una fucion asyncrona para crear el mapa
+    // creamos una fucion asyncrona para crear el mapa
     const crearMap = async () => {
-
-        if (view_actual == null) {
-            // 
-            const optiones: JimuMapViewConstructorOptions = ({
-                mapWidgetId: props.mapWidgetId,
-                dataSourceId: "",
-                mapViewManager: mapViewManager,
-                view: new MapView({
-                    container: containerMap.current,
-                    map: ({ basemap: "arcgis-streets-night" }),
-                    zoom: 1
-                })
-            });
-            
-            const { view } = await mapViewManager.createJimuMapView(optiones)
-            setView(view)
-
-            view.ui.add("Switch_Map3D", "top-right")
-            view.ui.add("Switch_MapBase", "top-right")
+        let map = null
+        if (check_Map3D === true) {
+            map = new SceneView({
+                container: containerMap.current,
+                map: ({ basemap: "arcgis-streets-night" }),
+                zoom: 1
+            })
         } else {
-            console.log("ya existe algun mapa debe de borrarlo con alguhn metodo")
+            map = new MapView({
+                container: containerMap.current,
+                map: ({ basemap: "arcgis-streets-night" }),
+                zoom: 1
+            })
+           
+
         }
 
+        const optiones: JimuMapViewConstructorOptions = ({
+            mapWidgetId: props.mapWidgetId,
+            dataSourceId: "",
+            mapViewManager: mapViewManager,
+            view: map
+        });
+
+        const { view } = await mapViewManager.createJimuMapView(optiones)
+        setView(view)
+        
+        const basemapgallery = new BasemapGallery({
+            view: view,
+
+        });
+        const expand = new Expand({
+            expandIconClass: "esri-icon-layer-list",
+            view: view,
+            content: basemapgallery,
+        });
+
+        view.ui.add(expand, "bottom-left");
+        view.ui.add("Switch_Map3D", "top-right")
+        view.ui.add("Switch_MapBase", "bottom-right")
     }
 
+
+    // cargamos el mapa al cargar el componente
+    useEffect(() => {
+        crearMap()
+    }, [check_Map3D]);
+
+
+    //    funciones para obtener el  checket del la configuracion
     async function get_check_Map3D() {
         try {
             let result = await props.config.getIn(["Map_3D"])
             if (result !== undefined) {
-                setCheck_Map3D(result)
+                setCheck_Config_Map(result)
             }
         } catch (error) {
-            console.log("error")
+            console.log("error" + error)
         }
     }
+
     async function get_check_MapBase() {
         try {
             let result = await props.config.getIn(["Map_Base"])
             if (result !== undefined) {
-                setCheck_Map3D(result)
+                setCheck_Config_BaseMap(result)
             }
         } catch (error) {
             console.log("error")
         }
     }
 
-
+    get_check_MapBase()
     get_check_Map3D()
 
 
+    // funciones de los manejadores del checket del mapa
 
+    const handleSwitch_Map3D = (event: ChangeEvent<HTMLInputElement>) => {
+        let check = event.target.checked
+        setCheck_Map3D(check)
 
-    useEffect(() => {
-        crearMap()
-    }, []);
-
-
+    }
+    // manejador de evento para el switch del mapa base
+    const handleSwitch_MapBase = (event: ChangeEvent<HTMLInputElement>) => {
+        let check = event.target.checked
+        setCheck_MapBase(check)
+    }
 
     return (
         <div className="containerMap" id="containerMap" ref={containerMap}>
-             
+
             <div id="Switch_Map3D" >
-                <p> {check_Map3D ?"<h1>gfdgdf</h1>" :"no paso"}</p>
+                {check_Config_Map === true &&
+                    <>
+                        <p className="Switch_p">Map 3D</p>
+                        <Switch onChange={handleSwitch_Map3D} checked={check_Map3D} ></Switch>
+                    </>
+                }
             </div>
 
             <div id="Switch_MapBase" >
-                <p className="Switch_p">Map Base</p>
-                <Switch ></Switch>
+                {check_Config_Basemap === true &&
+                    <>
+                        <p className="Switch_p">Map Base</p>
+
+                    </>
+                }
             </div>
+
         </div>
 
 
--- widgets/prueba/src/setting/setting.tsx
+++ widgets/prueba/src/setting/setting.tsx
@@ -1,5 +1,5 @@
 // modulos de jimu core 
-import { React, Immutable, UseDataSource, DataSourceTypes,ReactRedux} from 'jimu-core';
+import { React} from 'jimu-core';
 import { AllWidgetSettingProps } from 'jimu-for-builder';
 import { IMConfig } from '../config'
 
@@ -8,10 +8,6 @@ import { Switch } from 'jimu-ui'
 
 // modulos de react 
 import { useState, useEffect ,ChangeEvent} from 'react';
-// componente de propios de experiencie builder
-import { MapWidgetSelector } from 'jimu-ui/advanced/setting-components';
-import { DataSourceSelector } from 'jimu-ui/advanced/data-source-selector';
-
 // estilos css
 import "./aset/setting.css"
 
@@ -20,13 +16,13 @@ import "./aset/setting.css"
 export default function Setting(props: AllWidgetSettingProps<IMConfig>) {
 
 
-  const [check_Map3D, setCheck_Map3D] = useState(false)
-  const [check_MapBase, setCheck_MapBase] = useState(false)
+  // const [check_Map3D, setCheck_Map3D] = useState(false)
+  // const [check_MapBase, setCheck_MapBase] = useState(false)
 
 // manejador de evento para el switch del mapa 3D
   const handleSwitch_Map3D = (event: ChangeEvent<HTMLInputElement>) => {
     let check=event.target.checked
-    setCheck_Map3D(check)
+    
     props.onSettingChange({
       id: props.id,
       config: props.config.set("Map_3D",check) 
@@ -35,73 +31,28 @@ export default function Setting(props: AllWidgetSettingProps<IMConfig>) {
   // manejador de evento para el switch del mapa base
   const handleSwitch_MapBase = (event: ChangeEvent<HTMLInputElement>) => {
     let check=event.target.checked
-    setCheck_MapBase(check)
     props.onSettingChange({
       id: props.id,
       config: props.config.set("Map_Base",check) 
     })
-
   }
-  
-
-// creamos un objecto imutable para los datos que vamos a permitir para el witget
-  const supportedTypes = Immutable([DataSourceTypes.FeatureLayer]);
-
-  // creamos una funcion para  e
-  const onDataSourceSelected = (useDataSources: UseDataSource[]) => {
-    console.log('Selected data source:', useDataSources);
-    props.onSettingChange({
-      id: props.id,
-      useDataSources: useDataSources
-    });
-  }
-
-  // hace parte de los datos defiir o entender de una menjor manera 
-  const onMapWidgetSelected = (useMapWidgetIds: string[]) => {
-    props.onSettingChange({
-      id: props.id,
-      useMapWidgetIds: useMapWidgetIds
-    })
-  }
-
-  
+ 
   return (
-    
     <div className="p-3">
       <div className="setting" >
         <h6>Herramientas</h6>
         <div className='tool'>
           <span>Map 3D</span>
-          <Switch onChange={handleSwitch_Map3D} checked={check_Map3D} ></Switch>
+          <Switch onChange={handleSwitch_Map3D} checked={props.config.getIn(["Map_3D"])} ></Switch>
         </div>
 
         <div className='tool'>
           <span>Mapa base</span>
-          <Switch onChange={handleSwitch_MapBase} checked={check_MapBase}></Switch>
+          <Switch onChange={handleSwitch_MapBase} checked={props.config.getIn(["Map_Base"])}></Switch>
         </div>
 
       </div>
-
-      <div className="setting" >
-      <h6>Fuente de datos</h6>
-        <span> por favor seleciona Un mapa web, una escena web o una capa </span>
-        <DataSourceSelector
-          mustUseDataSource
-          types={supportedTypes}
-          useDataSources={props.useDataSources}
-          onChange={onDataSourceSelected}
-          widgetId={props.id}
-        />
-      </div>
-      <div className="widget-setting-demo">
-    <MapWidgetSelector
-      useMapWidgetIds={props.useMapWidgetIds}
-      onSelect={onMapWidgetSelected}
-    />
-  </div>
-
     </div>
-
   )
 
 }
